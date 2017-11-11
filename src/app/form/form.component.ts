d8b9bcc69c43bb6f65461e55e1727b9a7fc11cdf
Justyna Wojciechowska <justyna.wojciechowska04@gmail.com>
2017-11-11 19:53:27 +0100

    Adjusted styles, adedd aniamtion to background images

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-form',
-  templateUrl: './form.component.html'
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
+  formTitle = 'Take it all with you. Switch between devices, and pick up.';
+  @Input() gender: string;
+  public selectedGender = '';
 
+  radioSelected(value): void {
+      this.selectedGender = value;
+      console.log(this.selectedGender);
+  }
 }
